454f0fc766878b62d953b6e2071571193cb27be6
Roman <serzoom21@gmail.com>
2024-05-01 19:50:06 +0300

    Initial commit

--- traffic-lights-9/vite.config.js
+++ traffic-lights-9/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', 
+    assetsDir: 'src', 
     rollupOptions: {
-      input: {
-        main: 'src/main.jsx' 
-      }
+      input: '/src/App.jsx' 
     }
   }
 });
