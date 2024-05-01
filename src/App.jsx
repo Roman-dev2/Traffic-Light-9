f0449f5c023788a0fad742d21209dd2a2946eb0f
Roman <serzoom21@gmail.com>
2024-05-01 21:20:13 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,26 @@
 import './App.css';
-import TrafficLights from './TrafficLights';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import ClickDisplay from './ClickDisplay';
-import StatsBar from './StatsBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Home from './Pages/Home';
 import ErrorPage from './Pages/ErrorPage';
 import Header from './Header';
 import HorizontalTrafficLight from './HorizontalTrafficLight';
 import VerticalTrafficLight from './VerticalTrafficLight';
-
-
 import { TrafficLightsProvider } from './TrafficLightsProvider';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <><Header /><Outlet /></>,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: <Home />
-        },
-        {
-          path: "horizontal",
-          element: <HorizontalTrafficLight />
-        },
-        {
-          path: "vertical",
-          element: <VerticalTrafficLight />
-        }
-      ]
-    }
-  ]);
-
   return (
     <TrafficLightsProvider>
-      <RouterProvider router={router}>
+      <Router>
         <div className="App">
-          <StatsBar /> 
-          <ClickDisplay />
-          <TrafficLights />
+          <Header />
+          <Routes> 
+            <Route path="/" element={<Home />} />
+            <Route path="/horizontal" element={<HorizontalTrafficLight />} />
+            <Route path="/vertical" element={<VerticalTrafficLight />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
         </div>
-      </RouterProvider>
+      </Router>
     </TrafficLightsProvider>
   );
 }
